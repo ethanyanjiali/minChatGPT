d9943e98db33bcf4b14a995ef8880e458686c355
Ethan Yanjia Li <liyanjia92@gmail.com>
2023-03-23 21:44:13 -0700

    update train_rm to use lora by default

--- src/train_rm.py
+++ src/train_rm.py
@@ -75,7 +75,7 @@ def train_accelerate():
 
 def train(pretrain, batch_size, exp_name):
     device = 'cuda'
-    cfg = get_configs("gpt2-medium")
+    cfg = get_configs("gpt2-medium/lora")
     cfg.batch_size = batch_size
     cfg.pretrain = pretrain
     cfg.total_epochs = 1
